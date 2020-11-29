d9b1e98ec82e336ac6ddf7248a32b284c121a51a
E4ma <ifeomaidoko@gmail.com>
2020-11-29 04:03:33 -0700

    api signin commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Nav/navigation';
+import Signin from './components/Signin/signin';
+import Register from './components/Register/register';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
 import Rank from './components/Rank/rank';
+import FaceRecognition from './components/FaceRecognition/faceRecognition';
 import './App.css';
+
+const app = new Clarifai.App({
+    apiKey: 'fb506370a709416cab9fd9496cf66b44'
+});
 
 const particlesOptions = {
   "particles": {
@@ -67,19 +75,92 @@ const particlesOptions = {
       
   }
  
+  //we'll need to create a state so that our app knows what the value is that the user enters, remembers it and updates it anytime it gets changed
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            //what the user will input
+            input: ' ',
+            imageUrl: ' ',
+            box: {},
+            //box contains values we will receive
+            route: 'signin',
+            isSignedIn: false
+        }
+    }
+
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputImage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
+    
+    displayFaceBox =  (box) => {
+        this.setState({box: box});
+    }
+
+    onInputChange = (event) => {
+        //event listener...this is a property of the app
+        this.setState({input: event.target.value});
+        //this logs the text inputted
+    }
+
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models
+        .predict(
+            'c0c0ac362b03416da06ab3fa36fb58e3', 
+            this.state.input)
+        //error if you do this.state.imageUrl. Bad request 404
+        .then( response => this.displayFaceBox(this.calculateFaceLocation(response)))
+                //calculate face location with response data
+           .catch(err => console.log(err));
+    }
+
+    onRouteChange = (route) => {
+        if (route === 'signout'){
+            this.setState({isSignedIn: false})
+        } else if (route === 'home'){
+            this.setState({isSignedIn: true})
+        }
+        this.setState({route: route});
+    }
+
   render(){
+    //   const { isSignedIn, imageUrl, route, box } = this.state;
+    // then remove all the this.state here....this is destructuring
   return (
     <div className="App">
       <Particles className='particles'
                 params={particlesOptions} />
                 {/* because it is background + from react-particles-js npm */}
-   <Navigation />
-   <Logo />
+    
+   <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+   {this.state.route === 'home' 
+   ? <div> 
+    <Logo />
    <Rank />
-   <ImageLinkForm />
-   {/* {<FaceRecognition />} */}
+   <ImageLinkForm 
+   onInputChange={this.onInputChange} 
+   onButtonSubmit={this.onButtonSubmit}
+   />
+   <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+   </div>
+   :(
+       this.state.route === 'signin'
+       ? <Signin onRouteChange={this.onRouteChange} />
+       : <Register onRouteChange={this.onRouteChange} />
+   )
+   }
     </div>
   );
   }
--- src/components/ImageLinkForm/imageLinkForm.js
+++ src/components/ImageLinkForm/imageLinkForm.js
@@ -3,7 +3,7 @@ import './imageLinkForm.css';
 
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return(
         <div>
             <p className='f4'>
@@ -11,8 +11,11 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='form center pa3 br2 shadow-5'>
-                <input className='f4 pa2 w-70 center' type='text'></input>
-                <button className='w-40 grow f4 link ph3 pv2 dib dark bg-light-yellow'>Detect</button>
+                <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}></input>
+                <button 
+                className='w-40 grow f4 link ph3 pv2 dib dark bg-light-yellow'
+                onClick={onButtonSubmit}
+                >Detect</button>
                 </div>
             </div>
         </div>
